1f86f283508c16b9a0809ca04414400c764dd14f
Diego Dias Mól <diegodiasmol@hotmail.com>
2025-08-01 16:45:44 -0300

    Cadastro de Tarefas Básico React

---
+++ src/components/AddTask.jsx
@@ -0,0 +1,38 @@
+import Input from "./Input";
+import { useState } from "react";
+
+function AddTask({ onTaskAddClick }) {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  return (
+    <div className="space-y-4 p-6 bg-slate-200 rounded-md shadow  flex flex-col">
+      <Input
+        type="text"
+        placeholder="Digite o título da tarefa"
+        value={title}
+        onChange={(event) => setTitle(event.target.value)}
+      />
+      <Input
+        type="text"
+        placeholder="Digite a descrição da tarefa"
+        value={description}
+        onChange={(event) => setDescription(event.target.value)}
+      />
+      <button
+        onClick={() => {
+          if (!title.trim() || !description.trim()) {
+            return alert("Preencha os campos");
+          }
+          onTaskAddClick(title, description);
+          setTitle("");
+          setDescription("");
+        }}
+        className="w-full text-left bg-slate-400 text-white p-2 rounded-md"
+      >
+        Adicionar
+      </button>
+    </div>
+  );
+}
+
+export default AddTask;
---
+++ src/pages/DetailPage.jsx
@@ -0,0 +1,34 @@
+import { ChevronLeftIcon } from "lucide-react";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import Button from "../components/Button";
+import Title from "../components/Title";
+
+function DetailPage() {
+  const [searchParams] = useSearchParams();
+  const title = searchParams.get("title");
+  const description = searchParams.get("description");
+  const navigate = useNavigate();
+
+  return (
+    <div className="w-screen h-screen bg-slate-500  p-6">
+      <div className="w-[500] space-y-4">
+        <div className="flex justify-center relative mb-6">
+          <Button
+            onClick={() => navigate(-1)}
+            className=" text-white absolute left-0 top 0 bottom 0"
+          >
+            <ChevronLeftIcon></ChevronLeftIcon>
+          </Button>
+          <Title>Detalhes da Tarefa</Title>
+        </div>
+
+        <div className="bg-slate-200 p-4 rounded-md">
+          <h2 className="text-xl font-bold text-slate-600">{title}</h2>
+          <p className="text-slate-600">{description}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default DetailPage;
