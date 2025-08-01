e2027773ce0177f878e857e69a5be3a6db4e0e48
Diego Dias Mól <diegodiasmol@hotmail.com>
2025-08-01 17:04:12 -0300

    Correção do layout da tela de detalhes e adição de botão ao confirmar o encerrramento da tarefa

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -11,7 +11,7 @@ function DetailPage() {
 
   return (
     <div className="w-screen h-screen bg-slate-500  p-6">
-      <div className="w-[500] space-y-4">
+      <div className="w-[500] mx-auto space-y-4">
         <div className="flex justify-center relative mb-6">
           <Button
             onClick={() => navigate(-1)}
